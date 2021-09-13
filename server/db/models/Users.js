aa04f72e5d1493615f2cf40ef3bd1689d4e0465f
mpiech1813 <maciejpiech18@gmail.com>
2021-09-13 15:08:30 -0500

    added Users model

---
+++ server/db/models/Users.js
@@ -0,0 +1,30 @@
+const { STRING, BOOLEAN } = require('sequelize');
+const db = require('../db');
+
+const User = db.define(
+    'user',
+    {
+        userName: {
+            type: STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+            defaultValue: 'Anonymous Mouse',
+        },
+        email: {
+            type: STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                isEmail: true,
+            },
+        },
+        // adding for future admit tools
+        admin: {
+            type: BOOLEAN,
+            defaultValue: false,
+        },
+    },
+    { timestamp: false }
+);
