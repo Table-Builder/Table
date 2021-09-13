819e62a18236a89b58ff36773be7cb14a581e9fe
mpiech1813 <maciejpiech18@gmail.com>
2021-09-13 15:26:01 -0500

    added relations

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,13 @@
 const db = require('./db');
 // Import models here:
 const User = require('./models/Users');
+const Table = require('./models/Table');
+const Note = require('./models/Note');
 
 // Depencies go here:
+
+Table.belongsTo(User);
+User.hasMany(Table);
+
+Note.belongsTo(Table);
+Table.hasMany(Note);
