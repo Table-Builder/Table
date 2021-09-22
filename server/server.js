e20ec5b5296ab14eee5a9e607a871b5379194a7c
mpiech1813 <maciejpiech18@gmail.com>
2021-09-22 15:04:00 -0500

    added public folder and index.html

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const router = require('./api/router');
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/api', router);
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.use(express.json());
 
