404f2fa41313848ccf469e2be3fc06bc564df697
mpiech1813 <maciejpiech18@gmail.com>
2021-10-06 12:41:41 -0500

    added single note router

--- server/api/note.js
+++ server/api/note.js
@@ -4,10 +4,21 @@ const { Note } = require('../db/index');
 noteRouter.get('/', async (req, res, next) => {
     try {
         const notes = await Note.findAll();
-        console.log(notes);
+        res.send(notes);
     } catch (error) {
         console.log('error in notes router: ', error);
         next(error);
+    }
+});
+
+noteRouter.get('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const note = await Note.findByPk(id);
+        res.send(note);
+    } catch (error) {
+        console.log();
+        next(next);
     }
 });
 
