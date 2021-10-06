d9984800324361bc1534523ba5c11e01377ab421
mpiech1813 <maciejpiech18@gmail.com>
2021-10-06 11:59:47 -0500

    added the rout handler

--- server/api/router.js
+++ server/api/router.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRouter = require('./user');
 const notebookRouter = require('./notebook');
 const tableRouter = require('./table');
+const noteRouter = require('./note');
 
 router.use('/user', userRouter);
 router.use('/notebook', notebookRouter);
 router.use('/table', tableRouter);
+router.use('/note', noteRouter);
 
 module.exports = router;
