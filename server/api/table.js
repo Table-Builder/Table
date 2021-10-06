38dcbe71bc3b01ee56d5eed0a45c47110e05e1ee
mpiech1813 <maciejpiech18@gmail.com>
2021-10-06 11:51:01 -0500

    added route to tables and single table

--- server/api/table.js
+++ server/api/table.js
@@ -17,4 +17,22 @@ tableRouter.get('/', async (req, res, next) => {
     }
 });
 
+tableRouter.get('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const table = await Table.findByPk(id, {
+            include: [
+                {
+                    model: Note,
+                },
+            ],
+        });
+        console.log(table);
+        res.send(table);
+    } catch (error) {
+        console.log('error in single table, ', error);
+        next(error);
+    }
+});
+
 module.exports = tableRouter;
