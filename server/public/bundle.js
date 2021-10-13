f6c57d4b765e0885a6d78f1342339f99003cc0a4
mpiech1813 <maciejpiech18@gmail.com>
2021-10-12 20:06:44 -0500

    basic component is working

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -29907,12 +29907,14 @@ var __webpack_exports__ = {};
   !*** ./client/index.js ***!
   \*************************/
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
-/* harmony import */ var _components_App__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/App */ "./client/components/App.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+/* harmony import */ var _components_App__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/App */ "./client/components/App.js");
+
 
 
 const root = document.getElementById('root');
-react_dom__WEBPACK_IMPORTED_MODULE_0__.render( /*#__PURE__*/React.createElement(_components_App__WEBPACK_IMPORTED_MODULE_1__["default"], null), root);
+react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_App__WEBPACK_IMPORTED_MODULE_2__["default"], null), root);
 })();
 
 /******/ })()
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const router = require('./api/router');
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/api', router);
 app.get('/', (req, res, next) => {
