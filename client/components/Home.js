a9c8984ef33d55cfb37e2568aa5639f0a36036bd
mpiech1813 <maciejpiech18@gmail.com>
2021-10-13 12:34:24 -0500

    home skeleton created

---
+++ client/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        <div>
+            <h1>Hello from home page</h1>
+        </div>;
+    }
+}
+
+export default Home;
