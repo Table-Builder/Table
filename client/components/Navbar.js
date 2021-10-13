def7c2deca3681af21f2b0c1fe30670670fdcd5f
mpiech1813 <maciejpiech18@gmail.com>
2021-10-13 13:48:33 -0500

    some material ui for navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>This is the NavBar</h2>
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position='static'>
+                <Toolbar>
+                    <Button color='inherit'>Home</Button>
+                    <Button color='inherit'>Dashboard</Button>
+                    <Button color='inherit'>Log in</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 };
 
