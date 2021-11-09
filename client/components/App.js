22bcf869e8e48f24c91d435590846aee2961921e
mpiech1813 <maciejpiech18@gmail.com>
2021-11-09 14:31:13 -0600

    creating a theme

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@mui/material/styles';
 
 import Home from './Home';
 import Navbar from './Navbar';
@@ -15,12 +16,10 @@ class App extends Component {
 
     render() {
         return (
-            <div>
-                <Router>
-                    <Navbar />
-                    <Route exact path='/' component={Home} />
-                </Router>
-            </div>
+            <Router>
+                <Navbar />
+                <Route exact path='/' component={Home} />
+            </Router>
         );
     }
 }
